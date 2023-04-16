d051a5baa1f20e6edc654b7467f21c638b101a0a
IanaLois <ianaesteron@gmail.com>
2023-04-16 10:40:21 +0000

    Add function that retrieves a new random word

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 import random
 from words import word_list
+
+def new_word():
+    """
+    Retrieves a new randomised word from the imported word list
+    """
+    random_word = random.choice(word_list)
+    return random_word.upper()
